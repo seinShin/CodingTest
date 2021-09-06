ae903144aded88015f37c4ddce684afdcfcb0424
[seinShin] <tlstpdls13@gmail.com>
2021-09-06 13:56:38 +0900

    codingtest 깃헙 관리 테스트 커밋

---
+++ src/If/iftest1.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package If;
+
+/**************************************************
+* @FileName : iftest1.java
+* @Description:
+* @Author : se-in shin
+* @Version : 2021. 9. 6.
+* @Copyright : @Shinsein. All Right Reserved
+**************************************************/
+public class iftest1 {
+	public void main() {
+		System.out.println("테스트 해봅니다.");
+	}
+}
