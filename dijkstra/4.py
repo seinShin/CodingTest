42b5f2335a3608317daa04a75575bce82d24da1c
[seinShin] <tlstpdls13@gmail.com>
2023-05-19 17:11:59 +0900

    벨만 포드 알고리즘 플로이드 워셜 알고리즘

--- dijkstra/4.py
+++ dijkstra/4.py
@@ -13,7 +13,7 @@ for _ in range(e):
 for k in range(1, v+1):
     for i in range(1, v+1):
         for j in range(1, v+1):
-            graph[a][b] = min(graph[a][b] , graph[a][k]+graph[k][b])
+            graph[i][j] = min(graph[i][j] , graph[i][k]+graph[k][j])
             
             
 rst = INF
