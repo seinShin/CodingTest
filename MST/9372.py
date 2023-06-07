260117b673c027eef281d01d05a41a21909d515f
[seinShin] <tlstpdls13@gmail.com>
2023-06-07 11:41:35 +0900

    mst 연습

---
+++ MST/9372.py
@@ -0,0 +1,13 @@
+#9372 상근이의 여행
+import sys
+input = sys.stdin.readline
+
+for i in range(int(input())):
+    
+    n,m = map(int, input().split())
+    
+    for i in range(m):
+        a,b = map(int, input().split())
+        
+    print(n-1)
+
