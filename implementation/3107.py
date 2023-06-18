e0e4d05412b4509b26dd0457a0422adf884c51a3
[seinShin] <tlstpdls13@gmail.com>
2023-06-18 18:25:31 +0900

    문자열 구현 # 2469 사다리 타기 # 3107 IPv6

---
+++ implementation/3107.py
@@ -0,0 +1,27 @@
+# 3107 IPv6
+ipv6 = input()
+lst=ipv6.split(':')
+
+chk=[]
+for i in lst:
+    if i != '':
+        chk.append(i)
+cnt=0
+total=''
+for i in range(len(lst)):
+    if lst[i] =='' and cnt != 1:
+        total += '0000:'*(8-len(chk))
+        cnt=1   
+    elif lst[i] != '':
+        if len(lst[i])<4 :
+            total+= ('0'*(4-len(lst[i])))+lst[i]
+            total+=":"
+        else:
+            total+=lst[i] + ":"
+if len(total) == 40:
+    print(total[:-1])
+else:
+    print(total)
+
+     
+
