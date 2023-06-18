671b15e6a53bf97866619f55f86da78884583eb0
[seinShin] <tlstpdls13@gmail.com>
2023-06-18 14:55:06 +0900

    부스트캠프 예시 1

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
-from itertools import permutations, combinations
+#네이버 test
+arr=list(map(int, input().split()))
+arr.sort()
 
-lst = ['A', 'B', 'C']
+answer=[]
+tmp=arr[0]
+cnt=0
 
-for i in permutations(lst,2):
-    print("순열 :", i)
-    
-for i in combinations(lst,2):
-    print("조합 :", i)
+for i in range(1,len(arr)):
+    if tmp== arr[i]:
+        cnt+=1
+        if i == len(arr)-1:
+            answer.append(cnt+1)
+    else:
+        if cnt != 0:
+            answer.append(cnt+1)
+        tmp=arr[i]
+        cnt=0
+
+if len(answer) == 0:
+    print(-1)
+else:
+    print(answer)
