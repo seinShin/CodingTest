13d3d866890dc79a7c148f98007e0bfc67696d78
seinShin <tlstpdls13@gmail.com>
2023-10-11 16:10:46 +0900

    # 17070 파이프 옮기기 1 ** dfs/bfs 시초 -> dp 해결

---
+++ simulation/17070.py
@@ -0,0 +1,26 @@
+# 17070 파이프 옮기기 1
+import sys
+n = int(input())
+pipeline = [list(map(int, input().split())) for _ in range(n)]
+dp =[[[0 for _ in range(n)] for _ in range(n)] for _ in range(3)]
+
+
+# dp 방향, 행, 열
+dp[0][0][1] = 1
+for i in range(2, n):
+    # 1행은 가로만 가능
+    if pipeline[0][i] == 0:
+        dp[0][0][i] = dp[0][0][i-1]
+        
+for i in range(1, n):
+    for j in range(1, n):
+        if pipeline[i][j] == 0 and pipeline[i][j-1] == 0 and pipeline[i-1][j] == 0:
+            dp[2][i][j] = dp[0][i-1][j-1] + dp[1][i-1][j-1] + dp[2][i-1][j-1]
+            
+        if pipeline[i][j] == 0:
+            # 가로
+            dp[0][i][j] = dp[0][i][j-1] + dp[2][i][j-1]
+            # 세로
+            dp[1][i][j] = dp[1][i-1][j] + dp[2][i-1][j]
+        
+print(dp[0][n-1][n-1]+dp[1][n-1][n-1]+dp[2][n-1][n-1])
