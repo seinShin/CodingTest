a741e2fb113c1ab03af6dd0f4b13a61059f230cb
seinShin <tlstpdls13@gmail.com>
2024-06-17 21:53:57 +0900

    # 1952 달팽이 2

---
+++ codingTest_java/hd/hd_1952.java
@@ -0,0 +1,55 @@
+package codingTest_java.hd;
+
+import java.util.*;
+import java.io.*;
+public class hd_1952 {
+    static int N, M;
+    static int[][] map;
+
+    static int[] dx ={0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    static boolean[][] visited;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        M = sc.nextInt();
+        N = sc.nextInt();
+
+        map = new int[m][n];
+
+        int cnt = 0;
+        int sum = 0;
+
+        int idx = 0;
+        int nowX=0;
+        int nowY=0;
+
+        visited[nowX][nowY] = true;
+        sum=1;
+
+        while(idx < 4){
+            int nx= nowX+dx[idx];
+            int ny = nowY+dy[idx];
+
+            if(sum == N*M){
+                System.out.println(cnt);
+                return;
+            }
+            if(nx >= 0 && ny >= 0 && nx < M && ny < N && !visited[nx][ny]) {
+                sum++; // 지나온 칸 수 + 1
+                visited[nx][ny] = true; // 방문 처리
+                nowX = nx; // 다음 탐색을 위해 갱신
+                nowY = ny;
+            }
+
+            else{
+                idx++;
+                cnt++;
+            }
+
+            if(idx>=4){
+                idx=0;
+            }
+
+        }
+    }
+}
