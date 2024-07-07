d7928f760c72b2265549545873d092c8558b9f14
SEIN SHIN <81028514+seinShin@users.noreply.github.com>
2024-07-07 22:53:30 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 169.72 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(sb.append(a).append(b));
+    }
+}
