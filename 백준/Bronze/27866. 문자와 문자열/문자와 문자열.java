170ea1af48d9f133ea267ef78db9e5b7d07289a5
SEIN SHIN <81028514+seinShin@users.noreply.github.com>
2024-07-15 15:41:23 +0900

    [Bronze V] Title: 문자와 문자열, Time: 184 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class Main {
+    static int[][] arr = new int[101][101];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        int n = sc.nextInt();
+
+
+        System.out.println(word.charAt(n-1));
+
+
+    }
+}
+
