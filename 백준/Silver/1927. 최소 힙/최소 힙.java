5e49378365ba66c4421eb394bde4fb71622dc327
SEIN SHIN <81028514+seinShin@users.noreply.github.com>
2024-09-20 22:19:20 +0900

    [Silver II] Title: 최소 힙, Time: 724 ms, Memory: 29072 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.io.*;
+public class Main {
+
+    static int N;
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+
+        for(int i=0; i<N; i++){
+            int tmp = Integer.parseInt(bf.readLine());
+
+            if(tmp==0){
+                if(pq.isEmpty()){
+                    System.out.println("0");
+                }else{
+                    int x = pq.poll();
+                    System.out.println(x);
+                }
+            }else{
+                pq.offer(tmp);
+            }
+        }
+    }
+
+}
+
+
